7d24bd3430113e6acad4bbbd0228d07b73fec9fa
jmombiela <jmombiela@lyracons.com>
2021-06-14 00:32:12 -0300

    edit loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack, Heading, Image, CircularProgress} from "@chakra-ui/react";
+import { Stack, Heading, Image} from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
 const transitionValues = {
